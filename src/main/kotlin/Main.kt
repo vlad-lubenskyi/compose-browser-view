7349c1b7a7d671b72287b5395393b84edb1f3d50
Danil Didkovskiy <danylo.didkovskyi@teamdev.com>
2022-05-30 17:06:48 +0300

    Initialize mutableUrl in AddressBar function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -22,36 +21,36 @@ import com.teamdev.jxbrowser.engine.RenderingMode.OFF_SCREEN
 fun main() = application {
     val engine = Engine.newInstance(OFF_SCREEN)
     val browser = engine.newBrowser()
-
-    val startUrl = "https://www.google.com"
-    val mutableUrl = remember { mutableStateOf(startUrl) }
+    val url = "https://www.google.com"
 
     Window(onCloseRequest = ::exitApplication, title = "JxBrowser in Compose Desktop") {
         Column {
-            AddressBar(browser, mutableUrl)
+            AddressBar(browser, url)
             BrowserView(browser)
         }
     }
 
-    browser.navigation().loadUrl(startUrl)
+    browser.navigation().loadUrl(url)
 }
 
 @Composable
-private fun AddressBar(browser: Browser, url: MutableState<String>) {
+private fun AddressBar(browser: Browser, url: String) {
+    val mutableUrl = remember { mutableStateOf(url) }
+
     Row(
         modifier = Modifier.height(40.dp)
     ) {
         BasicTextField(
-            value = url.value,
+            value = mutableUrl.value,
             onValueChange = {
-                url.value = it
+                mutableUrl.value = it
             },
             modifier = Modifier.weight(1f).padding(start = 10.dp, top = 10.dp),
             singleLine = true
         )
         Button(
             modifier = Modifier.padding(3.dp),
-            onClick = { browser.navigation().loadUrl(url.value) }
+            onClick = { browser.navigation().loadUrl(mutableUrl.value) }
         ) {
             Text(text = "Go")
         }
